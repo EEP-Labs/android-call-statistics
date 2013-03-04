2f1361be0f09cd82c6311f662990bff1ded3feaf
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-03-04 23:36:27 +0100

    Initial import.

---
+++ src/org/ktln2/android/callstat/MainActivity.java
@@ -0,0 +1,56 @@
+package org.ktln2.android.callstat;
+
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.database.Cursor;
+import android.content.CursorLoader;
+import android.widget.SimpleCursorAdapter;
+import android.os.Bundle;
+import android.provider.CallLog.Calls;
+// FIXME: use support library
+
+
+public class MainActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    private SimpleCursorAdapter mAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAdapter = new SimpleCursorAdapter(
+            this, // context
+            android.R.layout.simple_list_item_1, // layout
+            null, // cursor
+            new String[] {
+                Calls.NUMBER
+            }, // from
+            new int[] {
+                android.R.id.text1
+            }
+        );
+        setListAdapter(mAdapter);
+
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        CursorLoader cl = new CursorLoader(
+            this,              // context
+            Calls.CONTENT_URI, // Uri for the Call log
+            null,              // projection
+            null,              // selection
+            null,              // selectionArgs
+            null               // sortOrder
+        );
+
+        return cl;
+    }
+
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mAdapter.swapCursor(cursor);
+    }
+
+    public void onLoaderReset(Loader<Cursor> loader) {
+    }
+}
